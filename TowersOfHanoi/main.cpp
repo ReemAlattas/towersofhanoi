a45c59592f2d1f193f4f2f0707c837085c4182c7
ReemAlattas <ralattas@live.com>
2016-07-23 02:59:35 +0300

    add function header

--- TowersOfHanoi/main.cpp
+++ TowersOfHanoi/main.cpp
@@ -8,8 +8,16 @@
 
 #include <iostream>
 
+using namespace std;
+
+void towersOfHanoi(int, int, int, int);
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
+}
+
+void towersOfHanoi(int no_disks, int initial_peg, int move_peg, int temp_peg) {
+    
 }
